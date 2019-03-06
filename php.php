3c519e321838a6355f951d5fddcf294305de22c0
Ana Radojicic <44723852+LilithFae@users.noreply.github.com>
2019-03-06 18:06:31 +0100

    PHP intro

---
+++ php.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Intro Hello World vezba</title>
+    <style>
+        h2 {
+            color: blueviolet;
+        }
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            font-family: "Verdana";
+            }
+    </style>
+</head>
+<body>
+    <h1>
+        Fajl sada cuvamo kao .php , ali i dalje moze sadrzati html kod.
+    </h1>
+    <?php    
+    //komentar
+    echo "<h2>Zdravo svete !</h2>";
+    echo 5+5;
+    /* Multi
+    line
+    komentar*/    
+    ?>
+</body>
+</html>
