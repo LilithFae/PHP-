bc1c5c9bf1c56a30629fdb5778b082129914b509
Ana Radojicic <44723852+LilithFae@users.noreply.github.com>
2019-03-06 18:04:49 +0100

    if else PHP

---
+++ if_else.php
@@ -0,0 +1,15 @@
+<?php
+$var = "test";
+if($var == "test") {
+    echo "prvo testiranje";
+} 
+elseif($var == "test1") {
+    echo "test drugi";
+} 
+elseif($var == "test2") {
+    echo "test treci";
+} 
+else {
+    echo "nedefinisana vrednost";
+}
+?>
